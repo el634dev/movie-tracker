19c79d8885c2f427cc850f019932eac18c747341
Dani Roxberry <dani@bitoriented.com>
2021-12-09 18:38:57 -0800

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from books_app.config import Config
+from app.config import Config
 import os
 
 app = Flask(__name__)
