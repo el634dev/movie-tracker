b6ee750757cc3b734e8162a39a9b8a5d456aa419
el634dev <84924260+el634dev@users.noreply.github.com>
2024-03-01 18:19:17 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,25 @@
 from app.extensions import db
 from flask_login import UserMixin
+import enum
+
+# # -------------------
+# class FormEnum(enum.Enum):
+#     """Helper class to make it easier to use enums with forms."""
+#     @classmethod
+#     def choices(cls):
+#         return [(choice.name, choice) for choice in cls]
+
+#     def __str__(self):
+#         return str(self.value)
+
+# -------------------
+# Audience
+# class Audience(FormEnum):
+#     """Audience Model"""
+#     CHILDREN = 'Children'
+#     YOUNG_ADULT = 'Young Adult'
+#     ADULT = 'Adult'
+#     ALL = 'All'
 
 #####################
 #    Models         #
@@ -10,17 +30,8 @@ class Movie(db.Model):
     """Movie model"""
     id = db.Column(db.Integer, primary_key=True)
     # Name of movie
-    name = db.Column(db.String(80), nullable=False, unique=True)
-    # Name of year
+    name = db.Column(db.String(80), nullable=False)
     year = db.Column(db.Date)
-
-    # The director 
-    director_id = db.Column(db.Integer, db.ForeignKey('director.id'), nullable=False)
-    # director = db.relationship('Director', back_populates='movies')
-
-    # The genres, e.g. horror, sci-fi, fantasy
-    # genres = db.relationship(
-    #     'Genre', secondary='movie_genre', back_populates='movies')
 
     def __str__(self):
         return f'<Movie: {self.name}>'
@@ -44,30 +55,6 @@ class Director(db.Model):
 
     def __repr__(self):
         return f'<Director: {self.name}>'
-
-
-# ------------------------
-# Genre Model
-class Genre(db.Model):
-    """Genre model"""
-    id = db.Column(db.Integer, primary_key=True)
-    # Genre Name
-    name = db.Column(db.String(80), nullable=False, unique=True)
-    # Movies associated with genre
-    # movies = db.relationship(
-    #     'Movie', secondary='movie_director')
-
-    def __str__(self):
-        return f'<Genre: {self.name}>'
-
-    def __repr__(self):
-        return f'<Genre: {self.name}>'
-
-
-movie_director_table = db.Table('movie_director',
-    db.Column('movie_id', db.Integer, db.ForeignKey('movie.id')),
-    db.Column('director_id', db.Integer, db.ForeignKey('director.id'))
-)
 
 # --------------------------------
 # User Model
