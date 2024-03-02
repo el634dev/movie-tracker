7c821fee990e4ec238df3a22e02c6758fe308d12
el634dev <84924260+el634dev@users.noreply.github.com>
2024-03-01 23:47:34 -0500

    Update routes.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 
-# from app.auth.forms import SignUpForm, LoginForm
-# from app.main.forms import User
+from app.auth.forms import LoginForm, SignUpForm
+from app.models import User
 
-# from app.extensions import app, db, bcrypt
+from app.extensions import app, db, bcrypt
 
 auth = Blueprint("auth", __name__)
 
@@ -13,37 +13,35 @@ auth = Blueprint("auth", __name__)
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     """Sign Up"""
-    pass
-    # form = SignUpForm()
+    form = SignUpForm()
 
-    # if form.validate_on_submit():
-    #     hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-    #     user = User(
-    #         username = form.username.data,
-    #         password = hashed_password
-    #     )
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(
+            username = form.username.data,
+            password = hashed_password
+        )
 
-    #     db.session.add(user)
-    #     db.session.commit()
-    #     flash('Account created.')
-    #     return redirect(url_for('auth.login'))
-    # return render_template('signup.html', form=form)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created.')
+        return redirect(url_for('auth.login'))
+    return render_template('signup.html', form=form)
 
 # -----------------------
 # Login a user
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     """Login in a user"""
-    pass
-    # form = LoginForm()
+    form = LoginForm()
 
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(username=form.username.data).first()
-    #     login_user(user, remember=True)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        login_user(user, remember=True)
 
-    #     next_page = request.args.get('next')
-    #     return redirect(next_page if next_page else url_for('main.homepage'))
-    # return render_template('login.html')
+        next_page = request.args.get('next')
+        return redirect(next_page if next_page else url_for('main.homepage'))
+    return render_template('login.html', form=form)
 
 # -----------------------
 # Logout a user
@@ -52,3 +50,4 @@ def logout():
     """logout user"""
     logout_user()
     return redirect(url_for('main.homepage'))
+
